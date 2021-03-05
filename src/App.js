d4e5163323054688a92bfa6c2156b375a751d9fc
jayyp35 <jprabhu35@gmail.com>
2021-03-05 17:07:42 +0530

    header+home+product rendered

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
   return (
     <div>
       {/* {Header} */}
       <Header/>
+      {/* {Home} */}
+      <Home/>
     </div>
   );
 }
