eb5bc2f253c3bae3f7db699ad7f123668628718b
jayyp35 <jprabhu35@gmail.com>
2021-03-05 23:23:48 +0530

    Added item counter in Sub-total

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useStateValue } from './StateProvider';
 import './Subtotal.css'
 
 function Subtotal() {
+    const [{basket},dispatch] = useStateValue();
+
     return (
         <div>
             <div className="subtotal-card">
-                <p>Subtotal (2 items): <strong>$100.00</strong></p> 
+                <p>Subtotal ({basket.length} items): <strong>$100.00</strong></p> 
                 <small><input className="subtotal-gift" type="checkbox"/>This order contains a gift</small>
                 <button>Proceed to Checkout</button>
             </div>
