ba297fcb471693c4dbefda92ec5c7af7a117c9eb
jayyp35 <jprabhu35@gmail.com>
2021-03-09 01:36:21 +0530

    component to show when data loads

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import BlurOnTwoToneIcon from '@material-ui/icons/BlurOnTwoTone';
+import './Loading.css'
+import React from 'react'
+
+function Loading() {
+    return (
+        <div className="loading">
+        <span className="icon"><BlurOnTwoToneIcon/></span>
+        </div>
+    )
+}
+
+export default Loading
